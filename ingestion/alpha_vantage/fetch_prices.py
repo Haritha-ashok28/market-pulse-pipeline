f50ebf394b4ff09dd38d48943b2e315dbc72c254
Haritha <haritha.works28299@gmail.com>
2026-05-15 14:14:31 -0400

    feat(ingestion): add logging to alpha vantage fetch script

--- ingestion/alpha_vantage/fetch_prices.py
+++ ingestion/alpha_vantage/fetch_prices.py
@@ -1,3 +1,14 @@
+import logging
+logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.FileHandler("logs/pipeline.log"),
+            logging.StreamHandler()
+        ]
+    )
+logger = logging.getLogger(__name__)
+
 import requests
 import time
 import json
@@ -11,25 +22,26 @@ API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
 BASE_URL = "https://www.alphavantage.co/query"
 
 tickers = {
-    "tech": ["NVDA", "GOOGL", "AAPL", "AMZN", "MSFT"],
-    "finance": ["JPM", "SPGI", "GS", "BAC", "V"],
-    "healthcare": ["JNJ", "UNH", "PFE", "ABBV", "MRK"]
-}
+        "tech": ["NVDA", "GOOGL", "AAPL", "AMZN", "MSFT"],
+        "finance": ["JPM", "SPGI", "GS", "BAC", "V"],
+        "healthcare": ["JNJ", "UNH", "PFE", "ABBV", "MRK"]
+    }
 
 for sector, stocks in tickers.items():
-    for i in stocks:
-        params = {
-            "function": "TIME_SERIES_DAILY",
-            "symbol": i,
-            "apikey": API_KEY
-            }
-        try:
-            response = requests.get(BASE_URL,params=params)
-            data = response.json()
-            os.makedirs(f"data/{sector}", exist_ok=True)
-            with open(f"data/{sector}/{i}.json", "w") as f:
-                json.dump(data,f)
-            time.sleep(12)
-        except Exception as e:
-            print(f"{i} failed: {e}")
-            continue
+        for i in stocks:
+            params = {
+                "function": "TIME_SERIES_DAILY",
+                "symbol": i,
+                "apikey": API_KEY
+                }
+            try:
+                response = requests.get(BASE_URL,params=params)
+                data = response.json()
+                os.makedirs(f"data/{sector}", exist_ok=True)
+                with open(f"data/{sector}/{i}.json", "w") as f:
+                    json.dump(data,f)
+                time.sleep(12)
+                logger.info(f"{i} fetched successfully")
+            except Exception as e:
+                logger.error(f"{i} failed: {e}")
+                continue
